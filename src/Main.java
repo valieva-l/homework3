ca358d743a58abb9279b5addd7e11c968ca56aac
lyays <l28075339@gmail.com>
2025-02-27 19:33:02 +0300

    hm3

--- src/Main.java
+++ src/Main.java
@@ -33,14 +33,23 @@ public class Main {
         byte annaSergeevna = 27;
         byte ekaterinaAndreevna = 30;
         short sheetsOfPaper = 480;
-        int numberOfSheets = sheetsOfPaper/(lyudmilaPavlovna+annaSergeevna+ekaterinaAndreevna);
+        int numberOfSheets = sheetsOfPaper / (lyudmilaPavlovna + annaSergeevna + ekaterinaAndreevna);
         System.out.println("На каждого ученика рассчитано " + numberOfSheets + " листов бумаги");
         // Задача 4
-
-
-
-
-
+        byte numberOfBottles = 16;
+        byte numberOfMinutes = 2;
+        byte minutes1 = 20;
+        int machinePerformance1 = minutes1 * (numberOfBottles / numberOfMinutes);
+        System.out.println("За 20 минут машина произвела " + machinePerformance1 + " штук бутылок");
+        short minutes2 = 1440;
+        int machinePerformance2 = minutes2 * (numberOfBottles / numberOfMinutes);
+        System.out.println("За сутки машина произвела " + machinePerformance2 + " штук бутылок");
+        short minutes3 = 4320;
+        int machinePerformance3 = minutes3 * (numberOfBottles / numberOfMinutes);
+        System.out.println("За 3 дня машина произвела " + machinePerformance3 + " штук бутылок");
+        int minutes4 = 44640;
+        int machinePerformance4 = minutes4 * (numberOfBottles / numberOfMinutes);
+        System.out.println("За 1 месяц машина произвела " + machinePerformance4 + " штук бутылок");
 
     }
 }
