b397179c15451293f5888d4ef395941296b059b4
lyays <l28075339@gmail.com>
2025-02-27 18:51:08 +0300

    hm3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Задача 1
+        int a = 200000;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        byte b = 99;
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        short c = 28453;
+        System.out.println("Значение переменной c с типом short равно " + c);
+        long d = -47857412;
+        System.out.println("Значение переменной d с типом long равно " + d);
+        float e = 2.9642f;
+        System.out.println("Значение переменной e с типом float равно " + e);
+        double f = -0.9878256358;
+        System.out.println("Значение переменной f с типом double равно " + f);
+        //Задача 2
+        float astra = 27.12f;
+        System.out.println(astra);
+        long adenium = 987678965549L;
+        System.out.println(adenium);
+        float lily = 2.786f;
+        System.out.println(lily);
+        short dandelion = 569;
+        System.out.println(dandelion);
+        short iris = -159;
+        System.out.println(iris);
+        short chamomile = 27897;
+        System.out.println(chamomile);
+        byte azalea = 67;
+        System.out.println(azalea);
+
     }
 }
